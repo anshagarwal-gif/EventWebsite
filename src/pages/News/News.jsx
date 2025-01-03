026cb5764f29cf70470cdf54296229d942c02067
Ayush9535 <ghodkeayush22@gmail.com>
2025-01-03 18:39:08 +0530

    news page updated

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -38,12 +38,21 @@ const News = () => {
           <img src={NewsImage} alt="newsimage" className='news-image' />
         </div>
         <p className='news-date'>01 JUNE 2024</p>
-        <h1 className='news-title'>Lorem ipsum dolor sit amet consectetur.</h1>
-        <p className='news-description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste id rerum fuga eos numquam tempora aliquid vero ipsum magnam, doloremque ad sunt minus hic laborum necessitatibus beatae enim reiciendis, deserunt voluptatem porro blanditiis? Cum amet quaerat illum vero quod necessitatibus voluptatem repellat beatae! Rerum, consequuntur repudiandae assumenda adipisci dolor ipsum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, laudantium nostrum? Deleniti dolorum sapiente odit voluptatem fugit reiciendis nihil quam? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus quo doloribus nobis consequatur asperiores magni aperiam, quibusdam nesciunt voluptatum aut quod similique deserunt. Dicta eligendi repudiandae facere qui autem, nemo sed? Nesciunt vero fugiat quos aspernatur, quidem iure. Necessitatibus rem consequatur suscipit, repellat dolorum esse maiores nobis molestias dignissimos fugiat! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque impedit laboriosam sint exercitationem non ex, repellat, eaque iure tempore numquam consequatur similique recusandae porro eius nobis blanditiis alias odio velit, fugiat dolores sit praesentium quia quibusdam dolor! Fugit, nemo reprehenderit animi, ipsam aut sint, a aspernatur neque nobis excepturi enim?</p>
+        <h1 className='news-title'>Unveiling Excellence in Events & Exhibitions</h1>
+        <p className='news-description'>Welcome to the latest edition of the Wadhwa Events & Exhibits newsletter, where we celebrate our milestones and showcase our commitment to creating extraordinary experiences for our esteemed clients. From corporate giants to educational institutions, we have left an indelible mark in the world of events and exhibits.</p>
+        <p className='news-description'>Our clientele speaks volumes about our expertise and dedication. We’ve delivered sophisticated and dynamic corporate event solutions for TATA, reflecting the brand’s innovation and legacy; curated elegant and expansive exhibitions for Sahara that showcased the company’s diverse portfolio; designed high-impact product launches and trade show exhibits for JCB to captivate global audiences; executed luxurious private events and bespoke exhibits for Pristine’s elite clientele; partnered with ICE Global for international conferences, ensuring flawless execution with a touch of finesse; and crafted stunning displays and promotional events for PNG Jewellers that radiated the brand’s elegance and tradition.</p>
+        <p className='news-description'>At MIT ADT University, we have managed a variety of impactful events that showcased the institution’s commitment to innovation and excellence. Our work included conceptualizing and executing academic summits, cultural fests, and technology expos that brought together students, faculty, and industry leaders. Notably, we had the honor of hosting and managing Persona, Pune's biggest techno-cultural event. With its dynamic mix of technology, culture, and creativity, Persona became a resounding success under our meticulous planning and seamless execution, further solidifying the university’s reputation as a hub of innovation and culture.</p>
+        <p className='news-description'>We take pride in our ability to adapt and innovate. Whether it’s crafting bespoke solutions for new-age startups or handling mega-events for established enterprises, Wadhwa Events & Exhibits continues to redefine industry standards. As we step into the future, our commitment to delivering excellence remains unwavering. With each project, we aim to inspire, innovate, and leave a lasting impression on our clients and their audiences.</p>
+        <p className='news-description'>Follow us on our journey to transform ideas into reality. For inquiries, partnerships, or to explore how we can make your vision come alive, reach out to us today.</p>
+
+        <div className='news-conclude'>
+          <h3>Wadhwa Events & Exhibits</h3>
+          <h3>Your Partner in Exceptional Event Experiences</h3>
+        </div>
       </div>
 
       <div className='section-divider'></div>
-
+{/* 
       <div className="related-updates">
         <h2 className="updates-heading">RELATED UPDATES</h2>
         <div className="updates-container">
@@ -86,7 +95,7 @@ const News = () => {
             Next
           </button>
         </div>
-      </div>
+      </div> */}
 
       <AppointmentComponent />
     </div>
